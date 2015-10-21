043565f53a451c53bd5318a84380f0e8d5bb741d
Chris Schlaeger <chris@linux.com>
2015-10-21 21:28:59 +0200

    Turn logger into a proper singleton and make it thread safe.

--- lib/fit4ruby/Log.rb
+++ lib/fit4ruby/Log.rb
@@ -10,7 +10,9 @@
 # published by the Free Software Foundation.
 #
 
+require 'monitor'
 require 'logger'
+require 'singleton'
 
 module Fit4Ruby
 
@@ -18,10 +20,30 @@ module Fit4Ruby
   # errors.
   class Error < StandardError ; end
 
-  class ILogger < Logger
+  # The ILogger class is a singleton that provides a common logging mechanism
+  # to all objects. It exposes essentially the same interface as the Logger
+  # class, just as a singleton and with some additional methods like 'fatal'
+  # and 'cricital'.
+  class ILogger < Monitor
 
+    include Singleton
+
+    @@logger = Logger.new(STDOUT)
+
+    # Pass all calls to unknown methods to the @@logger object.
+    def method_missing(method, *args, &block)
+      @@logger.send(method, args, &block)
+    end
+
+    # Make it properly introspectable.
+    def respond_to?(method, include_private = false)
+      @@logger.respond_to?(method)
+    end
+
+    # Print an error message via the Logger and exit the program with exit
+    # code 1.
     def fatal(msg)
-      super
+      @@logger.error(msg)
       exit 1
     end
 
@@ -35,7 +57,7 @@ module Fit4Ruby
 
   end
 
-  Log = ILogger.new(STDOUT)
+  Log = ILogger.instance
   Log.level = Logger::WARN
   Log.formatter = proc do |severity, time, progname, msg|
     msg + "\n"
